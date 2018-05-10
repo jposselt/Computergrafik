7ea7d55b9c3194abac7d964ae282f5a4e928ef06
Andre Matutat <32962412+AMatutat@users.noreply.github.com>
2018-05-10 20:23:56 +0200

    add Zoom

--- Blatt03/main.cpp
+++ Blatt03/main.cpp
@@ -26,7 +26,9 @@ glm::mat4x4 projection;
 
 float zNear = 0.1f;
 float zFar  = 100.0f;
-
+glm::vec3 eye(4.0f, 4.0f, 4.0f);
+glm::vec3 center(0.0f, 0.0f, 0.0f);
+glm::vec3 up(0.0f, 1.0f, 0.0f);
 /*
 Struct to hold data for object rendering.
 */
@@ -74,6 +76,7 @@ void initCube()
 		12, 13, 14, 12, 14, 15, // Left
 	    16, 17, 18, 16, 18, 19, // Top
 		20, 21, 22, 20, 22, 23  // Bottom
+	
 	};
 
 	GLuint programId = program.getHandle();
@@ -217,9 +220,7 @@ bool init()
 	glEnable(GL_DEPTH_TEST);
 
 	// Construct view matrix.
-	glm::vec3 eye(4.0f, 4.0f, 4.0f);
-	glm::vec3 center(0.0f, 0.0f, 0.0f);
-	glm::vec3 up(0.0f, 1.0f, 0.0f);
+
 
 	view = glm::lookAt(eye, center, up);
 
@@ -312,10 +313,12 @@ void glutKeyboard (unsigned char keycode, int x, int y)
 	  return;
 	  
 	case '+':
-		// do something
+		eye = eye + (0.1f, 0.1f, 0.1f);
+		view = glm::lookAt(eye, center, up);
 		break;
 	case '-':
-		// do something
+		eye = eye - (0.1f, 0.1f, 0.1f);
+		view = glm::lookAt(eye, center, up);
 		break;
 	case 'x':
 		// do something
