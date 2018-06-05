81fb54276fbd2a5bed50ca6bc8baef9bd36c2c89
Jonas Posselt <jonas.posselt@web.de>
2018-06-05 20:46:02 +0200

    More constants

--- Blatt04/Constants.h
+++ Blatt04/Constants.h
@@ -17,52 +17,78 @@ namespace Constants {
 	/* Axis tilt step size in degrees */
 	const double tiltStepSize = 1.0;
 
-	/* Speed factor step size */
-	const double speedStepSize = 0.5;
+	/* Maximum axis tilt (degrees) */
+	const double maxTilt = 360.0;
+
+	/* Minimum axis tilt (degrees) */
+	const double minTilt = 0.0;
+
+	/* Initial time scale factor */
+	const double initialTimeScaleFactor = 1.0;
+
+	/* Time scale factor step size */
+	const double timeScaleStepSize = 0.5;
 
 	/* Degrees in full circle */
 	const double degreeCircle = 360.0;
 	
+	/* Constants for the central body */
 	namespace Sun {
-		const double radius = 3.0;
+		const double radius = 2.0;
 		const double distance = 0.0;
 		const double orbitSpeed = 0.0;
 		const double rotationSpeed = 0.0;
 		const double tilt = 0.0; // degree
 		const double offset = 0.0;
-		glm::vec3 color = glm::vec3(1.0f, 1.0f, 0.0f);
+		const double startAngle = 0.0;
+		const glm::vec3 color = glm::vec3(1.0f, 1.0f, 0.0f);
 	}
 
+	/* Constants for the first planet */
 	namespace Planet_1 {
-
 		const double radius = 1.5;
 		const double distance = 5.0;
 		const double orbitSpeed = 30.0 / 1000.0;    // 30° per second;
 		const double rotationSpeed = 40.0 / 1000.0; // 40° per second
 		const double tilt = 0.0; // degree
 		const double offset = 0.0;
-		glm::vec3 color = glm::vec3(1.0f, 0.0f, 0.0f);
+		const double startAngle = 0.0;
+		const glm::vec3 color = glm::vec3(1.0f, 0.0f, 0.0f);
 
+		/* Constants for the first planets moons */
 		namespace Moons {
-			//const double radius = ;
-			//const double distance = ;
-			//const double orbitSpeed = ;
-			//const double offset = ;
+			const unsigned int nUpper = 0;
+			const unsigned int nLower = 0;
+			const unsigned int nCenter = 3;
+			const double radius = 0.5;
+			const double distance = 1.0;
+			const double orbitSpeed = 20.0 / 1000.0; // 20° per second
+			const double offset = 0.0;
+			const glm::vec3 color = glm::vec3(1.0f, 0.5f, 0.0f);
 		}
 	}
 
+	/* Constants for the second planet */
 	namespace Planet_2 {
-
 		const double radius = 2.0;
 		const double distance = 10.0;
 		const double orbitSpeed = 15.0 / 1000.0;    // 15° per second
 		const double rotationSpeed = 30.0 / 1000.0; // 30° per second
 		const double tilt = 45.0; // degree
 		const double offset = 0.0;
-		glm::vec3 color = glm::vec3(0.0f, 0.0f, 1.0f);
+		const double startAngle = 180.0;
+		const glm::vec3 color = glm::vec3(0.0f, 0.0f, 1.0f);
 
+		/* Constants for the second planets moons */
 		namespace Moons {
-
+			const unsigned int nUpper = 4;
+			const unsigned int nLower = 4;
+			const unsigned int nCenter = 2;
+			const double radius = 0.5;
+			const double distance = 1.0;
+			const double orbitSpeed = 20.0 / 1000.0; // 20° per second
+			const double offset = 2.0;
+			const glm::vec3 color = glm::vec3(0.5f, 0.0f, 1.0f);
 		}
 	}
 }
