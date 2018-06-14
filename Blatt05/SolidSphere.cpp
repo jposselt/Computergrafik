75d2f16954c2dcfb94653b1413b90f43d062e090
Jonas Posselt <jonas.posselt@web.de>
2018-06-14 18:21:48 +0200

    Klasse für Kugeln

---
+++ Blatt05/SolidSphere.cpp
@@ -0,0 +1,132 @@
+#include "SolidSphere.h"
+
+const glm::vec3 SolidSphere::defaultColor(0.8f, 0.8f, 0.8f);
+const double SolidSphere::defaultRadius(1.0);
+
+SolidSphere::SolidSphere(
+							cg::GLSLProgram & prog,
+							double radius,
+							glm::vec3 color,
+							unsigned int stacks,
+							unsigned int slices,
+							bool lighting
+						)
+	: VertexArrayObject(prog, lighting, SolidSphere::primitive),
+	radius(radius),
+	color(color),
+	nStacks(stacks),
+	nSlices(slices)
+{
+}
+
+SolidSphere::SolidSphere(cg::GLSLProgram & prog, double radius, glm::vec3 color, bool lighting)
+	: VertexArrayObject(prog, lighting, SolidSphere::primitive),
+	radius(radius),
+	color(color),
+	nStacks(SolidSphere::nStacksDefault),
+	nSlices(SolidSphere::nSlicesDefault)
+{
+}
+
+SolidSphere::SolidSphere(cg::GLSLProgram & prog, bool lighting)
+	: VertexArrayObject(prog, lighting, SolidSphere::primitive),
+	radius(SolidSphere::defaultRadius),
+	color(SolidSphere::defaultColor),
+	nStacks(SolidSphere::nStacksDefault),
+	nSlices(SolidSphere::nSlicesDefault)
+{
+}
+
+SolidSphere::SolidSphere(cg::GLSLProgram & prog)
+	: VertexArrayObject(prog, SolidSphere::defaultLighting, SolidSphere::primitive),
+	radius(SolidSphere::defaultRadius),
+	color(SolidSphere::defaultColor),
+	nStacks(SolidSphere::nStacksDefault),
+	nSlices(SolidSphere::nSlicesDefault)
+{
+}
+
+SolidSphere::~SolidSphere()
+{
+}
+
+std::vector<glm::vec3> SolidSphere::setVertices()
+{
+	std::vector<glm::vec3> vertices;
+	for (int i = 0; i <= nStacks; ++i) {
+
+		GLfloat V = i / (float)nStacks;
+		GLfloat phi = V * glm::pi <float>();
+
+		// Loop Through Slices
+		for (int j = 0; j <= nSlices; ++j) {
+
+			GLfloat U = j / (float)nSlices;
+			GLfloat theta = U * (glm::pi <float>() * 2);
+
+			// Calc The Vertex Positions
+			GLfloat x = cosf(theta) * sinf(phi);
+			GLfloat y = cosf(phi);
+			GLfloat z = sinf(theta) * sinf(phi);
+
+			// Push Back Vertex Data
+			vertices.push_back(glm::vec3(x * radius, y * radius, z * radius));
+		}
+	}
+	return vertices;
+}
+
+std::vector<glm::vec3> SolidSphere::setColors(const glm::vec3 & color)
+{
+	std::vector<glm::vec3> colors;
+	std::vector<glm::vec3> vertices;
+	for (int i = 0; i <= nStacks; ++i) {
+		// Loop Through Slices
+		for (int j = 0; j <= nSlices; ++j) {
+			colors.push_back(color);
+		}
+	}
+	return colors;
+}
+
+std::vector<glm::vec3> SolidSphere::setNormals()
+{
+	std::vector<glm::vec3> normals;
+	for (int i = 0; i <= nStacks; ++i) {
+
+		GLfloat V = i / (float)nStacks;
+		GLfloat phi = V * glm::pi <float>();
+
+		// Loop Through Slices
+		for (int j = 0; j <= nSlices; ++j) {
+
+			GLfloat U = j / (float)nSlices;
+			GLfloat theta = U * (glm::pi <float>() * 2);
+
+			// Calc The Vertex Positions
+			GLfloat x = cosf(theta) * sinf(phi);
+			GLfloat y = cosf(phi);
+			GLfloat z = sinf(theta) * sinf(phi);
+
+			// Push Back Vertex Data
+			normals.push_back(glm::normalize(glm::vec3(x, y, z)));
+		}
+	}
+	return normals;
+}
+
+std::vector<GLuint> SolidSphere::setIndices()
+{
+	std::vector<GLuint> indices;
+	for (int i = 0; i < nSlices * nStacks + nSlices; ++i) {
+
+		indices.push_back(i);
+		indices.push_back(i + nSlices + 1);
+		indices.push_back(i + nSlices);
+
+		indices.push_back(i + nSlices + 1);
+		indices.push_back(i);
+		indices.push_back(i + 1);
+	}
+	return indices;
+}
---
+++ Blatt05/SolidSphere.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "VertexArrayObject.h"
+class SolidSphere :
+	public VertexArrayObject
+{
+public:
+	SolidSphere(cg::GLSLProgram & prog, double radius, glm::vec3 color, unsigned int stacks, unsigned int slices, bool lighting);
+	SolidSphere(cg::GLSLProgram & prog, double radius, glm::vec3 color, bool lighting);
+	SolidSphere(cg::GLSLProgram & prog, bool lighting);
+	SolidSphere(cg::GLSLProgram & prog);
+	~SolidSphere();
+
+protected:
+	std::vector<glm::vec3> setVertices();
+	std::vector<glm::vec3> setColors(const glm::vec3& color);
+	std::vector<glm::vec3> setNormals();
+	std::vector<GLuint> setIndices();
+
+private:
+	double radius;
+	glm::vec3 color;
+	unsigned int nStacks;
+	unsigned int nSlices;
+
+	/* Class constants */
+	static const unsigned int nStacksDefault = 15;
+	static const unsigned int nSlicesDefault = 15;
+	static const double defaultRadius;
+	static const glm::vec3 defaultColor;
+	static const GLenum primitive = GL_TRIANGLES;
+	static const bool defaultLighting = false;
+};
+
