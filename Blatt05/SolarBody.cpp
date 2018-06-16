6cdb169d6eeed1ebf78aa47ab79c4e9ca3ef1efb
Jonas Posselt <jonas.posselt@web.de>
2018-06-16 20:16:59 +0200

    Klasse für Himmelskörper mit Hierarchie

---
+++ Blatt05/SolarBody.cpp
@@ -0,0 +1,98 @@
+#include <glm/gtc/matrix_transform.hpp>
+#include <cmath>       /* fmod */
+
+#include "SolarBody.h"
+#include "Constants.h"
+
+SolarBody::SolarBody(
+	cg::GLSLProgram & simple,
+	cg::GLSLProgram & lighting,
+	double distance,
+	double radius,
+	double orbitSpeed,
+	double rotationSpeed,
+	glm::vec3 color,
+	double axisTilt,
+	double yOffset,
+	double startAngle
+) 
+	: simpleShader(simple),
+	lightingShader(lighting),
+	distance(distance),
+	radius(radius),
+	orbitSpeed(orbitSpeed),
+	rotationSpeed(rotationSpeed),
+	color(color),
+	axisTilt(fmod(axisTilt, Constants::degreeCircle)),
+	yOffset(yOffset),
+	currentOrbitAngle(glm::radians<float>(startAngle)),
+	currentRotationAngle(0.0),
+	axis(true),
+	orbit(true)
+{
+	objectModel = new SolidSphere(lightingShader, radius, Constants::stacks, Constants::slices, true);
+	objectAxis = new Line(simpleShader, -Constants::axisScale * (float)radius * Constants::yAxis, Constants::axisScale * (float)radius * Constants::yAxis
+	);
+	objectOrbit = new Circle(simpleShader, distance);
+}
+
+SolarBody::~SolarBody()
+{
+	delete objectModel;
+	delete objectAxis;
+	delete objectOrbit;
+
+	for (SolarBody *sat : satellites) {
+		delete sat;
+	}
+}
+
+void SolarBody::init()
+{
+	objectModel->init(color);
+	objectAxis->init(color);
+	objectOrbit->init(color);
+}
+
+void SolarBody::render(glm::mat4x4 & model, glm::mat4x4 & view, glm::mat4x4 & projection, double time)
+{
+}
+
+void SolarBody::addSatellite(SolarBody *satellite)
+{
+	satellites.push_back(satellite);
+}
+
+void SolarBody::increaseAxisTilt(double value)
+{
+	if (axisTilt < Constants::maxTilt) {
+		axisTilt += value;
+	}
+}
+
+void SolarBody::decreaseAxisTilt(double value)
+{
+	if (axisTilt > Constants::minTilt) {
+		axisTilt -= value;
+	}
+}
+
+void SolarBody::increaseYOffset(double value)
+{
+	yOffset += value;
+}
+
+void SolarBody::decreaseYOffset(double value)
+{
+	yOffset -= value;
+}
+
+void SolarBody::drawAxis(bool value)
+{
+	axis = value;
+}
+
+void SolarBody::drawOrbit(bool value)
+{
+	orbit = value;
+}
---
+++ Blatt05/SolarBody.h
@@ -0,0 +1,56 @@
+#pragma once
+#include "Line.h"
+#include "Circle.h"
+#include "SolidSphere.h"
+#include "FlatSphere.h"
+
+class SolarBody
+{
+public:
+	SolarBody(cg::GLSLProgram & simple, cg::GLSLProgram & lighting, double distance, double radius, double orbitSpeed, double rotationSpeed, glm::vec3 color, double axisTilt, double yOffset, double startAngle);
+	~SolarBody();
+	void init();
+	void render(glm::mat4x4& model, glm::mat4x4& view, glm::mat4x4& projection, double time);
+	void addSatellite(SolarBody *satellite);
+
+	/* Adjust Parameters */
+	void increaseAxisTilt(double value);
+	void decreaseAxisTilt(double value);
+	void increaseYOffset(double value);
+	void decreaseYOffset(double value);
+	void drawAxis(bool value);
+	void drawOrbit(bool value);
+
+protected:
+	/* Program references */
+	cg::GLSLProgram & simpleShader;   // Shader for 1D objects (no lighting)
+	cg::GLSLProgram & lightingShader; // Shader for objects with lighting
+
+private:
+	/* Parameters */
+	double distance;
+	double radius;
+	double orbitSpeed;
+	double rotationSpeed;
+	double axisTilt;
+	double yOffset;
+	glm::vec3 color;
+
+	/* Instance Variables */
+	float currentOrbitAngle;
+	float currentRotationAngle;
+
+	/* Draw Options */
+	bool axis;
+	bool orbit;
+
+	/* Drawable Objects */
+	VertexArrayObject *objectModel, *objectAxis, *objectOrbit;
+
+	/* List of Satellite Objects */
+	std::vector<SolarBody*> satellites;
+
+	/* Length of axis compared to radius */
+	float axisScale = 3.0f;
+};
+
